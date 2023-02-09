dd8da9897e99d2079a392e6323ffad9e6d743368
Mios Stojsin <mios.stojsin@kelight.com>
2023-02-09 14:42:03 +0100

    fix(flights): Duplicates comparasion

--- src/flight/flight.util.ts
+++ src/flight/flight.util.ts
@@ -7,8 +7,14 @@ export class FlightUtil {
       .reduce((a, c) => a.concat(c), [])
       .reduce((unique, o) => {
         if (
-          !unique.some(
-            (obj) => JSON.stringify(obj.slices) === JSON.stringify(o.slices),
+          !unique.some((obj: Flight) =>
+            obj.slices.every((slice) => {
+              return o.slices.some(
+                (y) =>
+                  `${y.flight_number}${y.arrival_date_time_utc}${y.departure_date_time_utc}` ===
+                  `${slice.flight_number}${slice.arrival_date_time_utc}${slice.departure_date_time_utc}`,
+              );
+            }),
           )
         ) {
           unique.push(o);
